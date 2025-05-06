f4aed94b0563605a6e637ea7d64679f1f9e3dab0
MisterSnickles <papasnickles31@gmail.com>
2025-05-05 20:01:36 -0500

    Add project files.

---
+++ FinalProject/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.noteRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.selectedVarText = new System.Windows.Forms.Label();
+            this.selectedText = new System.Windows.Forms.Label();
+            this.bookDataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bookDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.saveButton);
+            this.groupBox3.Controls.Add(this.noteRichTextBox);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(601, 58);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(295, 432);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Saved Notes";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(183, 393);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(97, 29);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click_1);
+            // 
+            // noteRichTextBox
+            // 
+            this.noteRichTextBox.Location = new System.Drawing.Point(14, 25);
+            this.noteRichTextBox.MaxLength = 2000;
+            this.noteRichTextBox.Name = "noteRichTextBox";
+            this.noteRichTextBox.Size = new System.Drawing.Size(266, 362);
+            this.noteRichTextBox.TabIndex = 2;
+            this.noteRichTextBox.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.selectedVarText);
+            this.groupBox1.Controls.Add(this.selectedText);
+            this.groupBox1.Controls.Add(this.bookDataGrid);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(36, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(544, 432);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // selectedVarText
+            // 
+            this.selectedVarText.AutoSize = true;
+            this.selectedVarText.Location = new System.Drawing.Point(137, 397);
+            this.selectedVarText.Name = "selectedVarText";
+            this.selectedVarText.Size = new System.Drawing.Size(0, 20);
+            this.selectedVarText.TabIndex = 4;
+            // 
+            // selectedText
+            // 
+            this.selectedText.AutoSize = true;
+            this.selectedText.Location = new System.Drawing.Point(16, 397);
+            this.selectedText.Name = "selectedText";
+            this.selectedText.Size = new System.Drawing.Size(115, 20);
+            this.selectedText.TabIndex = 3;
+            this.selectedText.Text = "Selected book:";
+            // 
+            // bookDataGrid
+            // 
+            this.bookDataGrid.AllowUserToAddRows = false;
+            this.bookDataGrid.AllowUserToDeleteRows = false;
+            this.bookDataGrid.AllowUserToResizeColumns = false;
+            this.bookDataGrid.AllowUserToResizeRows = false;
+            this.bookDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.bookDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.bookDataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.bookDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bookDataGrid.Location = new System.Drawing.Point(20, 25);
+            this.bookDataGrid.MultiSelect = false;
+            this.bookDataGrid.Name = "bookDataGrid";
+            this.bookDataGrid.ReadOnly = true;
+            this.bookDataGrid.RowHeadersVisible = false;
+            this.bookDataGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.bookDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.bookDataGrid.Size = new System.Drawing.Size(501, 351);
+            this.bookDataGrid.TabIndex = 0;
+            this.bookDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bookDataGrid_CellContentClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Search for a book:";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(507, 26);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 26);
+            this.searchButton.TabIndex = 9;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click_1);
+            // 
+            // searchBar
+            // 
+            this.searchBar.Location = new System.Drawing.Point(284, 30);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.Size = new System.Drawing.Size(217, 20);
+            this.searchBar.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Max: 2000 Characters";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 538);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBar);
+            this.Name = "Form1";
+            this.Text = "Online Book Journal";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bookDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RichTextBox noteRichTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label selectedVarText;
+        private System.Windows.Forms.Label selectedText;
+        private System.Windows.Forms.DataGridView bookDataGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FinalProject/Form1.cs
@@ -0,0 +1,202 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // **************************************************************************************
+        // ** naming convention (database : snake_case, class : PascalCase, local : camelCase) **
+        // **************************************************************************************
+
+
+        public class BookResult
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Year { get; set; }
+        }
+
+        public class OpenLibraryResponse
+        {
+            public List<Doc> Docs { get; set; }
+        }
+
+        public class Doc
+        {
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("author_name")]
+            public List<string> AuthorName { get; set; }
+
+            [JsonProperty("first_publish_year")]
+            public int? FirstPublishYear { get; set; }
+        }
+
+        // new content ========================================================================
+
+        private void SetSelectedTextFromRow(int rowIndex)
+        {
+            if (rowIndex >= 0)
+            {
+                selectedVarText.Text = bookDataGrid.Rows[rowIndex].Cells[0].Value?.ToString();
+            }
+        }
+        private void bookDataGrid_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            SetSelectedTextFromRow(e.RowIndex);
+
+            string selectedTitle = bookDataGrid.Rows[e.RowIndex].Cells["Title"].Value.ToString();
+
+            // Optional: Get author too for better uniqueness
+            string selectedAuthor = bookDataGrid.Rows[e.RowIndex].Cells["Author"].Value.ToString();
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OnlineBooks.mdf;Integrated Security=True;";
+            string query = "SELECT notes FROM JournalEntries WHERE book_title = @title AND book_author = @author";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@title", selectedTitle);
+                    cmd.Parameters.AddWithValue("@author", selectedAuthor);
+
+                    conn.Open();
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        noteRichTextBox.Text = result.ToString(); // make sure this is your control name
+                    }
+                    else
+                    {
+                        noteRichTextBox.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load saved notes: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            if (bookDataGrid.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a book to save", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OnlineBooks.mdf;Integrated Security=True;";
+
+            string query = "INSERT INTO JournalEntries (book_title, book_author, book_year, notes) " +
+                           "VALUES (@title, @author, @year, @notes)";
+
+            // identify user selected row
+            var selectedRow = bookDataGrid.SelectedRows[0];
+
+            // assign variables to each column in selected row and convert each value into string
+            string title = selectedRow.Cells["Title"].Value.ToString();
+            string author = selectedRow.Cells["Author"].Value.ToString();
+            string year = selectedRow.Cells["Year"].Value.ToString();
+            string notes = noteRichTextBox.Text;
+
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@title", title);
+                    cmd.Parameters.AddWithValue("@author", author);
+                    cmd.Parameters.AddWithValue("@year", year);
+                    cmd.Parameters.AddWithValue("@notes", notes);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Journal entry saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to save entry: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private async void searchButton_Click_1(object sender, EventArgs e)
+        {
+            string query = searchBar.Text.ToLower();
+
+            if (string.IsNullOrEmpty(query))
+            {
+                throw new Exception(string.Empty);
+            }
+
+            // api link
+            string url = $"https://openlibrary.org/search.json?q={Uri.EscapeDataString(query)}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    // send request to Open Library API url
+                    string response = await client.GetStringAsync(url);
+
+                    // converts JSON text into OpenLibraryResponse object so (Title : List<string> AuthorName : PublishYear)
+                    var result = Newtonsoft.Json.JsonConvert.DeserializeObject<OpenLibraryResponse>(response);
+
+                    var books = result.Docs
+                    .Select(book => new BookResult
+                    {
+                        Title = book.Title,
+                        Author = book.AuthorName?.FirstOrDefault() ?? "Unknown",
+                        Year = book.FirstPublishYear ?? 0
+                    })
+                    .ToList();
+
+                    bookDataGrid.DataSource = null; // clear existing bindings
+                    bookDataGrid.AutoGenerateColumns = true;
+                    bookDataGrid.DataSource = books;
+                    bookDataGrid.Columns["Title"].Width = 200;
+                    bookDataGrid.Columns["Author"].Width = 200;
+                    bookDataGrid.Columns["Year"].Width = 60;
+
+                    MessageBox.Show($"Amount of books found: {books.Count}");
+
+                    bookDataGrid.Update();
+                    bookDataGrid.Refresh();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
